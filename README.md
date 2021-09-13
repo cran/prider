3a077340dce448d50920d0454fa5471a93d55ffc
Manu Tamminen <mavatam@utu.fi>
2021-09-13 06:30:02 +0000

    version 1.0.2

---
+++ README.md
@@ -0,0 +1,39 @@
+# Prider
+
+Prider permits multiplexed oligonucleotide primer and probe design for 
+complex DNA sequence sets by implementing an algorithm for linearly 
+scaling approximation of set coverage. A detailed description available at
+[Smolander and Tamminen, 2021](https://www.biorxiv.org/content/10.1101/2021.09.06.459073v1).
+
+## Installation
+
+```R
+# install.packages("devtools")
+devtools::install_github("manutamminen/prider")
+
+```
+
+## Examples
+
+```R
+test_fasta <- system.file("extdata", "test.fasta", package = "prider")
+
+# Runs Prider with default values
+primer_designs <- prider(test_fasta)
+
+# Returns all the primers
+primers(primer_designs)
+
+# Returns the primers of a specific primer group
+primers(primer_designs)[1]
+
+# Returns all the sequences
+sequences(primer_designs)
+
+# Returns the sequence of a specific Id
+sequences(primer_designs)[1]
+
+# Plots the primers groups and the target sequences as a heatmap
+plot(primer_designs)
+
+```
---
+++ src/chunker.cpp
@@ -0,0 +1,68 @@
+#include <Rcpp.h>
+#include <string>
+#include <vector>
+
+using namespace Rcpp;
+
+std::vector< std::string > sliding_window(std::string sequence, int window_size)
+{
+  int win_size{ window_size - 1 };
+  std::vector<char> chars( sequence.begin(), sequence.end() );
+  int num_chunks( chars.size() - win_size );
+  std::vector< std::string > acc;
+  for (int i=0; i < num_chunks; ++i)
+    acc.push_back(sequence.substr(i, win_size));
+  return acc;
+}
+
+//' Sliding window to create chunks of DNA sequences
+//'
+//' @title chunker
+//' @description Creates all primer candidates for a group of sequences using a
+//'              sliding window.
+//' @param seq_table A DataFrame containing a column for sequence ids (Id) and
+//'        sequences (Seq).
+//' @param window_size An integer. Set the sliding window width.
+//' @return A DataFrame containing columns for the sequence ids (Id),
+//'         indexes (Ix), joined ids and indexes (Id_Ix), and the primer
+//'         sequences (Seq).
+//'
+//' @examples
+//'
+//' test_csv <- system.file("extdata", "test.csv", package = "prider")
+//'
+//' test_csv <- read.csv(test_csv)
+//'
+//' chunks <- chunker(test_csv)
+//'
+//' @export
+// [[Rcpp::export]]
+DataFrame chunker(DataFrame seq_table,
+                  int window_size = 20)
+{
+  std::vector< std::string > ids = seq_table["Id"];
+  std::vector< std::string > seqs = seq_table["Seq"];
+
+  std::vector< std::string > seq_chunk_acc;
+  std::vector< std::string > seq_id_acc;
+  std::vector< std::string > seq_ix_acc;
+  std::vector< std::string > seq_id_ix_acc;
+
+  for (int inc1 = 0; inc1 < seqs.size(); ++inc1) {
+    std::vector< std::string > seq_acc(sliding_window(seqs[inc1], window_size));
+    std::string seq_id = ids[inc1];
+    for (int inc2 = 0 ; inc2 < seq_acc.size() ; ++inc2) {
+      seq_chunk_acc.push_back(seq_acc[inc2]);
+      seq_id_acc.push_back(seq_id);
+      seq_ix_acc.push_back(std::to_string(inc2 + 1));
+      seq_id_ix_acc.push_back(seq_id + "_" + std::to_string(inc2 + 1));
+    }
+  }
+
+  return DataFrame::create(Named("Id") = seq_id_acc,
+                           Named("Ix") = seq_ix_acc,
+                           Named("Id_Ix") = seq_id_ix_acc,
+                           Named("Seq") = seq_chunk_acc);
+}
+
+
